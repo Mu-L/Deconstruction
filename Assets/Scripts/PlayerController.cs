0d2a1e89b491e034ca95fc839f33545194b2903a
zheng shuo MacBook <zs8861@gmail.com>
2020-11-05 22:37:18 +0800

    初步完成核心玩法实现

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //public float speed;
+    public float distanceToGo;
+    public float coldTime;
+    private float time;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        time = coldTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DeleteByDeltaTime();
+    }
+
+    void DeleteByDeltaTime()
+    {
+        if (coldTime > 0)
+        {
+            coldTime -= Time.deltaTime;
+        }
+        else
+        {
+            //do something
+            //Vector3 newPos = transform.position + new Vector3(distanceToGo, 0, 0);
+            if(WallCheck.isRight)
+            {
+                transform.localRotation = Quaternion.Euler(0, 0, 0);
+                transform.position = transform.position + new Vector3(distanceToGo, 0, 0);
+            }
+            else
+            {
+                transform.localRotation = Quaternion.Euler(0, 180, 0);
+                transform.position = transform.position + new Vector3(-distanceToGo, 0, 0);
+            }
+            
+            coldTime = time;
+        }
+    }
+}
---
+++ Assets/Scripts/WallCheck.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallCheck : MonoBehaviour
+{
+    public static bool isRight;
+
+    private static int i;
+    private bool isInNewRoom;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isRight = true;
+        i = 0;
+        isInNewRoom = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        //isRight = !isRight;
+        if (!isInNewRoom)
+        {
+            isRight = !isRight;
+        }
+        else
+        {
+            isRight = true;
+            isInNewRoom = false;
+        }
+
+        Debug.Log("Player离开了房间");
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(i > 0)
+        {
+            if (other.gameObject.CompareTag("Room"))
+            {
+                Debug.Log("Player进入了房间");
+
+                isInNewRoom = true;
+            }
+        }
+        i++;
+    }
+}
